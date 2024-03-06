7021d9d43a129725971f2420489f8bc9576d8e23
U-BKM\aherbay <aherbay@MAHMETERBAY.bkm.local>
2024-03-06 13:58:11 +0300

    script updated

--- devops.sh
+++ devops.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if test -f ./source.txt; then
+	echo "source file exists"
+else touch ./source.txt
+fi
+
+if test -f ./target.txt; then
+	echo "target file exists"
+else touch ./target.txt
+fi
+
 if test -f ./transfer.sh; then
     ./transfer.sh
 else python3 ./main.py
