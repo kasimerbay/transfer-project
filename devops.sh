944ec4e0e7a696fc4634f297e1080f857dc29b2f
U-BKM\aherbay <aherbay@MAHMETERBAY.bkm.local>
2024-03-12 10:48:24 +0300

    project vol.1

--- devops.sh
+++ devops.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if test -f ./source.txt; then
-        > source.txt
-else 
+if test -f ./source.txt
+then
+    > source.txt
+else
 	touch ./source.txt
 fi
 
-if test -f ./target.txt; then
-        > target.txt
-else 
+if test -f ./target.txt
+then
+    > target.txt
+else
 	touch ./target.txt
 fi
 
-if test -f ./transfer.sh; then
-    	> transfer.sh && echo "Old transfer data has been deleted now.\nPlease update the source and target repos"
-else 
+if test -f ./transfer.sh
+then
+    > transfer.sh && echo "Old transfer data has been deleted now.\nPlease update the source and target repos"
+else
 	python3 ./main.py
 fi
 
