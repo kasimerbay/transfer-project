3f9f0518922dfb0b3137afa5fc74c34c78edad0b
Ahmet Kasım Erbay <aherbay@bkm.local>
2024-05-22 15:59:47 +0300

    project parametrized

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ This project is created for project transfer accross SCM Tool instances.
 """
 
 from module import *
+import sys
+from dels.bitbucket_json import repos
 
 def main():
 
-    source_list = read_file("source.txt")
-    target_list = read_file("target.txt")
+    user = sys.argv[1]
+    key = sys.argv[2]
+    source_scm = sys.argv[3]
+    target_scm = sys.argv[4]
 
-    create_commands(source_list, target_list)
+    repo_names = get_repos(repos)
+
+    clone_repos(source_scm, key, repo_names, user)
+
+    push_repos(target_scm, key, repo_names, user)
+
+    delete_files()
 
 if __name__ == "__main__":
     main()
