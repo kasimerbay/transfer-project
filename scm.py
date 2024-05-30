9e5b964be5f8bb485c544b6c4500422948c71d38
U-BKM\aherbay <aherbay@MAHMETERBAY.bkm.local>
2024-05-30 14:16:57 +0300

    delete repo and project; create repo and project features added

--- scm.py
+++ scm.py
@@ -4,10 +4,12 @@ This library is created to transfer projects across Bankalararası Kart Merkezi 
 By Ahmet Kasım Erbay: 20.02.2024 - 16:17:25
 """
 
+from queue import Empty
 import subprocess,os
 
 
 class Instance:
+    print("Welcome!, You'r privileged to work with the Atlassian Prince... Enjoy the show :)", end="\n\n")
     def __init__(self, instance, user):
         self.instance = instance
         self.user = user
@@ -39,6 +41,7 @@ class Instance:
         return subprocess.run(command.split(" "), cwd=".", stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode("utf-8")
 
     def run_post(self, command):
+        
         stream = os.popen(command)
         output = stream.read().strip()
         print(output)
@@ -54,19 +57,25 @@ class Project(Instance):
 
     def list_repositories(self):
         command = f"curl -k -u{self.user} https://{self.instance}/rest/api/1.0/projects/{self.key}/repos?limit=100"
+        try:
 
-        api_call = self.run(command)
-        api_call = self.api_to_json(api_call)
+            api_call = self.run(command)
+            api_call = self.api_to_json(api_call)
 
-        return [api_call["values"][i]["name"] for i in range(int(api_call["size"]))]
+            return [api_call["values"][i]["name"] for i in range(int(api_call["size"]))]
+        except:
+            return []
 
     def mirror_repos(self, repo_list, target_scm):
 
         target = Project(instance=target_scm, user=self.user, key=self.key)
-        repository_list = target.list_repositories()
 
         error_list = []
+        # If target does not have the project, this for loop creates the project
+        if target.key not in target.list_projects():
+            target.create_project()
 
+        repository_list = target.list_repositories()
         # If target has some missing repositories, this loop creates them
         for repo_name in repo_list:
             if repo_name not in repository_list:
@@ -92,6 +101,46 @@ class Project(Instance):
         repository_list = self.list_repositories()
 
         return self.mirror_repos(repo_list=repository_list, target_scm=target_scm)
+
+    def create_project(self):
+        
+        PROJECT_URL = f'https://{self.instance}/rest/api/latest/projects'
+        DICT = dict()
+        create_project = """curl --request POST -u %s --url '%s' --header 'Accept:application/json' --header 'Content-Type:application/json' --data '{"key":"%s","avatarUrl":"","avatar":"","links": %s}'"""%(self.user, PROJECT_URL, self.key ,DICT)
+
+        try:
+            self.run_post(create_project)
+        except:
+            raise Exception(f"Could not create {self.key} on {self.instance}.")
+
+
+    def delete_project(self):
+
+        PROJECT_DELETE_URL = f'https://{self.instance}/rest/api/latest/projects/{self.key}'
+        delete_project = """curl --request DELETE -u '%s' --url '%s'"""%(self.user, PROJECT_DELETE_URL)
+
+        try:
+            repo_list = self.list_repositories()
+
+            if repo_list:
+                print(f"\nFollowing repositories will be deleted\n\n")
+
+                for repo_name in repo_list:
+                    print(repo_name)
+
+            if repo_list:
+                for repo_name in repo_list:
+                    Repository(instance=self.instance, user=self.user, key=self.key, repo_name=repo_name).delete_repository()
+
+                    if repo_list.index(repo_name) == len(repo_list) - 1:
+                        self.run_post(delete_project)
+                        print("Project deleted")
+            else:
+                self.run_post(delete_project)
+                print("Project deleted")
+
+        except:
+            raise Exception(f"Could not delete {self.key} on {self.instance}. It may not be exists or {self.user[:self.user.index(':')]} does not have admin permission, or your project is not empty.")
 
 
 class Repository(Project):
@@ -126,3 +175,10 @@ class Repository(Project):
             self.run_post(command)
         except:
             raise Exception(f"There is a problem occured in creation of {self.repo_name} on {self.instance}.")
+
+    def delete_repository(self):
+
+        REPOSITORY_DELETE_URL = f'https://{self.instance}/rest/api/latest/projects/{self.key}/repos/{self.repo_name}'
+        delete_repo = """curl --request DELETE -u %s --url '%s' --header 'Accept:application/json'"""%(self.user, REPOSITORY_DELETE_URL)
+
+        self.run_post(delete_repo)
