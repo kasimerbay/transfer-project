65ea1d9cbda9478edf940686b58c04396c9a3c27
U-BKM\aherbay <aherbay@MAHMETERBAY.bkm.local>
2024-06-22 09:24:18 +0300

    Commits added to methods

--- scm.py
+++ scm.py
@@ -14,7 +14,7 @@ class Admin:
         self.user = user
 
     def delete_all_projects(self):
-        
+
         project_keys = Instance(instance=self.instance, user=self.user).list_projects()
         undeleted_projects = []
 
@@ -38,7 +38,7 @@ class Admin:
             if group_name != 'stash-users' and group_name != 'DevOps':
                 try:
                     Group(instance=self.instance, user=self.user, group_name=group_name).delete_group()
-                    
+    
                 except:
                     undeleted_groups.append(group_name)
             else:
@@ -85,6 +85,18 @@ class Instance:
         self.user = user
 
     def api_to_json(self, api_call):
+        """Returns Json formatted API Call to SCM
+
+            This is a complementary function for API Call
+        Args:
+            api_call (_type_): Curl command string result to be formatted to Dict
+
+        Raises:
+            Exception: If the user has no permission on target object
+
+        Returns:
+            Dict: Formatted dictionary object from API Call
+        """
         words = {"true":"True", "false":"False"}
 
         for key,value in words.items():
@@ -98,10 +110,26 @@ class Instance:
         return api_call
 
     def run(self, command):
+        """Run on system with Git installed
+
+        Args:
+            command (string): git command to be run
+
+        Returns:
+            None: _
+        """
         print(command)
         return subprocess.run(command.split(" "), cwd=".", stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode("utf-8")
 
     def run_post(self, command):
+        """Run on system with git
+
+        Args:
+            command (string): git command to be run. This command runs on a Linux OS.
+
+        Returns:
+            String: String to be sent to api_to_json method.
+        """
         print(command)
         stream = os.popen(command)
         output = stream.read().strip()
@@ -109,7 +137,11 @@ class Instance:
         return output
 
     def list_projects(self):
+        """List project keys in an Bitbucket Instance
 
+        Returns:
+            List(string): List of project keys in the instance
+        """
         command = f"curl -k -u'{self.user}' https://{self.instance}/rest/api/1.0/projects?limit=100"
 
         api_call = self.run_post(command)
@@ -118,7 +150,11 @@ class Instance:
         return [api_call["values"][i]["key"] for i in range(int(api_call["size"]))]
 
     def project_list(self):
-        # command = f"curl -k -u'{self.user}' https://{self.instance}/rest/api/1.0/projects?limit=100"
+        """List of API calls to projects
+
+        Returns:
+            List(string): List of API calls to projects. This method is a powerset of list_projects method.
+        """
         list_projects = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/projects?limit=200' --header 'Accept:application/json' """
 
         api_call = self.run_post(list_projects)
@@ -128,7 +164,11 @@ class Instance:
         return [(projects[i]["key"], projects[i]["name"], projects[i]["id"]) for i in range(len(projects))]
 
     def list_all_groups(self):
-
+            """ List all groups in a Bitbucket Instance
+            
+            Returns:
+                List(string): List of all the group names in a Bitbucket Instance
+            """
             list_groups = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/admin/groups?limit=1600' --header 'Accept:application/json' """
             api_call = self.run_post(list_groups)
             api_call = self.api_to_json(api_call)
@@ -138,6 +178,11 @@ class Instance:
             return groups
 
     def list_users(self):
+        """List all users in a Bitbucket Instance
+        
+        Returns:
+            List(string): List of all users in a Bitbucket Instance
+        """
         command = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/users?limit=1000' --header 'Accept:application/json'"""
 
         api_call = self.run_post(command)
@@ -146,6 +191,11 @@ class Instance:
         return [api_call["values"][i]["name"] for i in range(int(api_call["size"]))]
 
     def user_list(self):
+        """List of API calls to users
+
+        Returns:
+            List(string): List of API calls to users. This method is a powerset of list_users method.
+        """
         command = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/users?limit=1000' --header 'Accept:application/json'"""
 
         api_call = self.run_post(command)
@@ -156,6 +206,15 @@ class Instance:
         return [(user["name"], user["displayName"], user["emailAddress"]) for user in users]
 
     def create_groups(self):
+        """Create ideal groups based on project keys in an Instance
+
+            This method is an ideal method to standardize a Bitbucket Instance. It create groups with 
+            <KEY>_ADMIN, <KEY>_WRITE and <KEY>_READ for all the groups in the Instance. This groups
+            is to be privilaged for the <KEY> project, accordingly.
+
+        Returns:
+            None: _
+        """
         project_list = self.list_projects()
 
         for project_key in project_list:
@@ -164,7 +223,13 @@ class Instance:
                 Group(instance=self.instance, user=self.user, group_name=group_name).create_group()
 
     def give_group_permissions(self):
-        
+        """Give ideal group permissions to projects.
+
+            Based on the groups created by create_groups() method, give permissions to these groups accordingly.
+
+        Returns:
+            None: _
+        """
         projects = Instance(instance=self.instance, user=self.user).list_projects()
         self.create_groups()
 
@@ -177,18 +242,34 @@ class Group(Instance):
         super().__init__(instance, user)
 
     def create_group(self):
+        """Create a group from Group object
+        
+        Returns:
+            None: _
+        """
 
         create_group = f"""curl -u'{self.user}' --request POST --url 'https://{self.instance}/rest/api/latest/admin/groups?name={self.group_name}' --header 'Accept:application/json' -H "X-Atlassian-Token:no-check" """
 
         self.run_post(create_group)
 
     def delete_group(self):
+        """Delete a group
+
+        Returns:
+            None: _
         
+        """
         delete_group = f"""curl -u'{self.user}' --request DELETE --url 'https://{self.instance}/rest/api/latest/admin/groups?name={self.group_name}' --header 'Accept:application/json' -H "X-Atlassian-Token:no-check" """
 
         self.run_post(delete_group)
 
     def mirror_all_groups(self, target_scm):
+        """Mirror or transfer all the groups within a Bitbucket Instance to another Bitbucket Instance
+
+        Args:
+            target_scm (String): Domain name for your Bitbucket Instance.
+                Example: "bitbucket-prod.example.com"
+        """
         group_list_source = self.list_all_groups()
         group_list_target = Instance(instance=target_scm, user=self.user).list_all_groups()
 
@@ -208,7 +289,11 @@ class Group(Instance):
             print(group_name)
 
     def list_all_group_permissions(self):
+        """List all groups with matching permissions for all the groups in an instance
 
+        Returns:
+            Dict(Dict): Dictionary of {<KEY>:{<GROUP>:<PERMISSON>}} 
+        """
         projects = self.list_projects()
         group_permissions = {}
 
@@ -226,17 +311,34 @@ class User(Instance):
         super().__init__(instance, user)
 
     def create_user(self):
+        """Create a user in a given instance
+        
+        Returns:
+            None: _
+        """
         create_user = f"""curl -u'{self.user}' --request POST --url 'https://{self.instance}/rest/api/latest/admin/users?emailAddress={self.email}&password={self.password}&displayName={self.display_name}&name={self.name}' -H "X-Atlassian-Token:no-check" """
 
         self.run_post(create_user)
 
     def delete_user(self):
+        """Delete a user
+        
+        Returns:
+            None: _
+        """
         delete_user = f"""curl -u'{self.user}' --request DELETE --url 'https://{self.instance}/rest/api/latest/admin/users?name={self.name}' --header 'Accept:application/json' """
 
         self.run_post(delete_user)
 
     def mirror_all_users(self, target_scm):
+        """Mirror or transfer all users in a Bitbucket instance to another.
 
+        Args:
+            target_scm (String): https address of the Instance
+
+        Raises:
+            Exception: User creation failure
+        """
         user_list_source = Instance(instance=self.instance, user=self.user).user_list()
         user_list_target = Instance(instance=target_scm, user=self.user).list_users()
 
@@ -258,14 +360,17 @@ class User(Instance):
         for name in untransferred_users:
             print(name)
 
-
 class Project(Instance):
     def __init__(self, instance, user, key):
         self.key = key
         super().__init__(instance, user)
 
     def list_repositories(self):
+        """List repositories in a given project
 
+        Returns:
+            List(String): List of repository names in the project
+        """
         list_repositories = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/projects/{self.key}/repos?limit=100' --header 'Accept:application/json' """
         try:
             api_call = self.run_post(list_repositories)
@@ -276,7 +381,14 @@ class Project(Instance):
             return []
 
     def user_permissions(self):
+        """List of user names and permissions in a project
+
+            This method lists individual users that has permissions as user not involved in a group!
+
+            Returns:
+                List(list): List of name and permission pair in a list
         
+        """
         project_user_permissions = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/projects/{self.key}/permissions/users' --header 'Accept:application/json'"""
 
         api_call = self.run_post(project_user_permissions)
@@ -285,7 +397,12 @@ class Project(Instance):
         return [(api_call["values"][i]["user"]["name"],api_call["values"][i]["permission"]) for i in range(int(api_call["size"]))]
 
     def mirror_repos(self, repo_list:list, target_scm):
+        """Mirror or transfer individual repositories accross Bitbucket Instances
 
+        Args:
+            repo_list (list): list of repositories that are subject to transfer
+            target_scm (String): https address of target instance
+        """
         target = Project(instance=target_scm, user=self.user, key=self.key)
 
         error_list = []
@@ -301,14 +418,20 @@ class Project(Instance):
             if repo_name not in repository_list:
                 Repository(instance=target_scm, user=self.user, key=self.key, repo_name=repo_name).create_repository()
 
+        untracked_repos = [ "acquierissuer_poc", "grpc_loadtest_app", "switch-connector-service_poc", "switch-core-service_poc", "switch-redis-timeout", "thales-ps-hsm", "tr-switch-artifacts", "tr-switch-couchbase-dcp-poc", "tr-switch-couchbase-kafka-connect-poc", "tr-switch-couchbase-multinode-poc", "tr-switch-couchbase-spring-data-poc","tr-switch-net-cap"]
+
         for repo_name in repo_list:
-            try:
-                repo = Repository(self.instance, self.user, self.key, repo_name)
-                repo.clone()
-                repo.push(target_scm=target_scm)
-            except:
-                error_list.append(f"{repo_name} -- " + "Clone or Push Error!")
-                continue
+            if repo_name not in untracked_repos:
+                try:
+                    repo = Repository(self.instance, self.user, self.key, repo_name)
+                    repo.clone()
+                    repo.push(target_scm=target_scm)
+                except:
+                    error_list.append(f"{repo_name} -- " + "Clone or Push Error!")
+                    continue
+            else:
+                error_list.append(f"{repo_name} -- " + "Repository is told as not to be transfered")
+
 
         if len(error_list)!= 0:
             print("\n\n------ List of untransferred repositories:")
@@ -317,12 +440,24 @@ class Project(Instance):
             print(f"Total: {len(error_list)}")
 
     def mirror(self, target_scm):
+        """Mirror or tansfer an entire project
 
+        Args:
+            target_scm (String): https address of target instance
+
+        Returns:
+            _type_: _description_
+        """
         repository_list = self.list_repositories()
 
         return self.mirror_repos(repo_list=repository_list, target_scm=target_scm)
 
     def create_project(self):
+        """Create project for given Project object
+        
+        Exception:
+            Project creation failure
+        """
 
         PROJECT_URL = f'https://{self.instance}/rest/api/latest/projects'
         DICT = dict()
@@ -334,7 +469,10 @@ class Project(Instance):
             raise Exception(f"Could not create {self.key} on {self.instance}.")
 
     def delete_project(self):
-
+        """Delete an entire project
+        
+            This method deletes all the repositories along with the container project
+        """
         PROJECT_DELETE_URL = f'https://{self.instance}/rest/api/latest/projects/{self.key}'
         delete_project = """curl --request DELETE -u '%s' --url '%s'"""%(self.user, PROJECT_DELETE_URL)
 
@@ -362,7 +500,11 @@ class Project(Instance):
             raise Exception(f"Could not delete {self.key} on {self.instance}. It may not be exists or {self.user[:self.user.index(':')]} does not have admin permission, or your project is not empty.")
 
     def list_group_permission(self):
-
+        """List groups and permissions assigned to a project
+        
+        Returns:
+            List(list): List of group and permission pair list for a project
+        """
         project_group_permissions = f"""curl -u'{self.user}' --request GET --url 'https://{self.instance}/rest/api/latest/projects/{self.key}/permissions/groups?limit=300' --header 'Accept:application/json' """
 
         api_call = self.run_post(project_group_permissions)
@@ -371,6 +513,11 @@ class Project(Instance):
         return [(api_call["values"][i]["group"]["name"],api_call["values"][i]["permission"]) for i in range(int(api_call["size"]))]
 
     def give_group_permission(self):
+        """Create ideal groups for a project
+        
+        Returns:
+            None: _
+        """
         groups = Instance(instance=self.instance, user=self.user).list_all_groups()
 
         PERMISSIONS = ["PROJECT_ADMIN", "PROJECT_WRITE", "PROJECT_READ"]
@@ -396,6 +543,34 @@ class Project(Instance):
 
             print("\nCheck if project KEY is right. Or the groups exist")
 
+    def mirror_groups(self, target_scm):
+        """Mirror or transfer all the groups within a project to another Bitbucket instance
+
+        Args:
+            target_scm (String): Domain name for your Bitbucket Instance.
+                Example: "bitbucket-prod.example.com"
+        """
+        source_project_groups = self.list_group_permission()
+        target_project_groups = Project(instance=target_scm, user=self.user, key=self.key).list_group_permission()
+
+        untransferred_groups = []
+
+        for group in source_project_groups:
+            if group not in target_project_groups:
+                try:
+                    Group(instance=target_scm, user=self.user, group_name=group[0]).create_group()
+                except:
+                    untransferred_groups.append(group[0])
+            else:
+                untransferred_groups.append(group[0])
+        
+        if len(untransferred_groups) != 0:
+            print()
+            print("These are the groups that are not transferred;", end="\n")
+            for group in untransferred_groups:
+                print(group[0])
+            print()
+
 class Repository(Project):
     def __init__(self, instance, user, key, repo_name):
         self.repo_name = repo_name
@@ -411,7 +586,7 @@ class Repository(Project):
     def push(self, target_scm):
 
         push_ = f"https://{self.user}@{target_scm}/scm/{self.key.lower()}/{self.repo_name}.git"
-        command = f"git -C repos/{self.repo_name} push {push_} --all"
+        command = f"git -C repos/{self.repo_name} push --force {push_} --all"
 
         self.run_post(command)
 
